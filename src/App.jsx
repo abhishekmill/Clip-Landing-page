c92a45fb9e94ff43cece773c479f8ebb4d9ba2e3
Abhishek Verma <114756516+abhishekmill@users.noreply.github.com>
2025-04-19 17:50:19 +0530

    added mobile limitations

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Slide from "./components/Slide";
 
 function App() {
+  const [isDesktop, setIsDesktop] = useState(true);
+
+  useEffect(() => {
+    const checkDevice = () => {
+      setIsDesktop(window.innerWidth > 768);
+    };
+
+    checkDevice();
+    window.addEventListener("resize", checkDevice);
+    return () => window.removeEventListener("resize", checkDevice);
+  }, []);
+
   return (
     <div className=" h-screen flex  bg-gray-800 ">
-
-      <Slide />
+      {isDesktop ? (
+        <Slide />
+      ) : (
+        <div className=" text-white capitalize w-screen text-center h-screen flex-col flex justify-center items-center ">
+          <h2 className="p-5"> please use Desktop or Bigger screen Device </h2>
+          <a
+            href="https://github.com/abhishekmill"
+            target="_blank"
+            className="p-5 text-green-400"
+          >
+            {" "}
+            github.com/abhishekmill
+          </a>
+        </div>
+      )}
     </div>
   );
 }
